9d19f302cfe90756f80888b3948b0a06862c20ac
HaroldoC <carvalhoh@gmail.com>
2023-06-07 21:36:56 -0300

    Update

---
+++ Arguments vs Parameters.py
@@ -0,0 +1,7 @@
+# Parameters are the names used when defining a function or a method, and into the parentheses.
+def say_hello(name, emoji):
+    print(f"Hello {name} {emoji}")
+
+
+# Arguments are the values passed to the function when it is called.
+say_hello("Alex", "😊")
---
+++ Functions.py
@@ -0,0 +1,7 @@
+def say_hello(name, emoji):
+    print(f"Hello {name} {emoji}")
+
+
+say_hello("Alex", "😊")
+say_hello("Alex", "😊")
+say_hello("Alex", "😊")
